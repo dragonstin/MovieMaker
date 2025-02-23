ab3a498ea0e9da6dcee11f5f58c913e5167ba582
Fernanda Dagostin <nanda.dagostin@gmail.com>
2025-02-23 13:25:59 -0500

    Update text.js
    - Update the Watson authentication
    - Update to use the Wikipedia lib

--- robots/text.js
+++ robots/text.js
@@ -1,17 +1,24 @@
-const algorithmia = require('algorithmia')
-const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
+// const algorithmia = require('algorithmia')
+// const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentenceBoundaryDetection = require('sbd')
 
-const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const getFromWikipedia = require('./wikipedia')
+
+const watson = require('../credentials/watson-nlu.json')
+const Assistantv2 = require('ibm-watson/assistant/v2.js')
+const { IamAuthenticator } = require('ibm-watson/auth')
 const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1.js')
 
 const nlu = new NaturalLanguageUnderstandingV1({
-    iam_apikey: watsonApiKey,
-    version: '2018-04-05',
-    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api'
+    version: '2024-07-05',
+    authenticator: new IamAuthenticator({
+        apikey : watson.apikey
+    }),
+    serviceUrl: watson.url
 })
 
 const state = require('./state.js')
+const { response } = require('express')
 
 async function robot() {
     console.log('> [text-robot] Stating...')
@@ -26,10 +33,11 @@ async function robot() {
     state.save(content)
     
     async function fetchContentFromWikipedia(content) {
-        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
-        const wikipediaAlgorithm = algorithmiaAuthenticated.algo("web/WikipediaParser/0.1.2?timeout=300")
-        const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm)
-        const wikipediaContent = wikipediaResponde.get()
+        // const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+        // const wikipediaAlgorithm = algorithmiaAuthenticated.algo("web/WikipediaParser/0.1.2?timeout=300")
+        // const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm)
+        // const wikipediaContent = wikipediaResponde.get()
+        const wikipediaContent = await getFromWikipedia(content.searchTerm)
 
         content.sourceContentOriginal = wikipediaContent.content
         console.log('> [text-robot] Fetching done!')
